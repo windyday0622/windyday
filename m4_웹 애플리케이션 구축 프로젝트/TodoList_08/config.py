8f4b06a41bdbdba6e0f8d987e01149d22c4a8646
windyday0622 <167948754+windyday0622@users.noreply.github.com>
2024-07-17 09:13:38 +0900

    Add files via upload

---
+++ m4_웹 애플리케이션 구축 프로젝트/TodoList_08/config.py
@@ -0,0 +1,4 @@
+SECRET_KEY = '09d26bba5aff12ec002bdbd3234a24d03ce1436c56987b3a'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///tasks.db'
+SQLALCHEMY_DATABASE_URI = "mysql://hyunjin:Monster98!@localhost:3306/hyunjin_db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
